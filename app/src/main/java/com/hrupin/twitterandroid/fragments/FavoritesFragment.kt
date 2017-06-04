d892bdfb574b0dabab0041bf7294ab41264448ba
Igor Khrupin <igor@hrupin.com>
2017-06-04 10:24:13 +0200

    added stub fragments

---
+++ app/src/main/java/com/hrupin/twitterandroid/fragments/FavoritesFragment.kt
@@ -0,0 +1,17 @@
+package com.hrupin.twitterandroid.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by Igor Khrupin www.hrupin.com on 6/4/17.
+ */
+class FavoritesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/hrupin/twitterandroid/fragments/SearchFragment.kt
@@ -0,0 +1,20 @@
+package com.hrupin.twitterandroid.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by Igor Khrupin www.hrupin.com on 6/4/17.
+ */
+
+class SearchFragment : android.support.v4.app.Fragment() {
+
+    override fun onCreateView(inflater: android.view.LayoutInflater?, container: android.view.ViewGroup?, savedInstanceState: android.os.Bundle?): android.view.View? {
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
